ec716e46db568f2ec968d617a1ddff615cf7c68e
machadomatt <matheusmacsyn@gmail.com>
2020-10-11 01:15:27 -0300

    Code

---
+++ README.md
@@ -0,0 +1,25 @@
+# Slide Stories
+
+A simple Instagram-like stories slide with HTML, CSS and Javascript following Origamid's tutorial on [Youtube](https://www.youtube.com/watch?v=VaSr6uixmb0).
+
+## Getting Started
+
+### Dependencies
+
+There is no dependencies :smiley:
+
+### Installing
+
+Just download or clone the repository.
+
+## Author
+
+[Matheus Machado](https://github.com/machadomatt)
+
+## License
+
+Slide Stories is licensed under the [MIT license](https://opensource.org/licenses/MIT)
+Photo *cat1.png* by [Ramiz Dedaković](https://unsplash.com/@ramche?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText) on [Unsplash](https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText)
+Photo *cat2.png* by [Biel Morro](https://unsplash.com/@bielmorro?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText) on [Unsplash](https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText)
+Photo *cat3.png* by [Dzmitry Dudov (Dead__Angel_)](https://unsplash.com/@dead__angel_?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText) on [Unsplash](https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText)
+Photo *cat4.png* by [Vadim Sadovski](https://unsplash.com/@vadimsadovski?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText) on [Unsplash](https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText)
---
+++ slide-stories.js
@@ -0,0 +1,63 @@
+class SlideStories {
+    /** @param {string} id */
+    constructor(id) {
+        this.slide = document.querySelector(`[data-slide=${id}]`)
+        this.active = 0
+        this.init()
+    }
+
+    /** @param {Number} index */
+    activeSlide(index) {
+        this.active = index
+        this.items.forEach((item) => item.classList.remove('active'))
+        this.items[index].classList.add('active')
+        this.thumbItems.forEach((item) => item.classList.remove('active'))
+        this.thumbItems[index].classList.add('active')
+        this.autoSlide()
+    }
+
+    next() {
+        if (this.active < this.items.length - 1) {
+            this.activeSlide(this.active + 1)
+        } else {
+            this.activeSlide(0)
+        }
+    }
+
+    prev() {
+        if (this.active > 0) {
+            this.activeSlide(this.active - 1)
+        } else {
+            this.activeSlide(this.items.length - 1)
+        }
+    }
+
+    addNavigation() {
+        const nextBtn = this.slide.querySelector('.slide-next')
+        const prevBtn = this.slide.querySelector('.slide-prev')
+        nextBtn.addEventListener('click', this.next)
+        prevBtn.addEventListener('click', this.prev)
+    }
+
+    addThumbItems() {
+        this.items.forEach(() => (this.thumb.innerHTML += `<span class="slide-thumb-item"></span>`))
+        this.thumbItems = Array.from(this.thumb.children)
+    }
+
+    autoSlide() {
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(this.next, 5000)
+    }
+
+    init() {
+        this.next = this.next.bind(this)
+        this.prev = this.prev.bind(this)
+        this.items = this.slide.querySelectorAll('.slide-items > *')
+        this.thumb = this.slide.querySelector('.slide-thumbs')
+        this.addThumbItems()
+        this.activeSlide(0)
+        this.addNavigation()
+    }
+}
+
+new SlideStories('slide')
